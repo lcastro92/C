889a9728525e2e6698adda0411d7153679b51025
Lucas <lucas.castro.0307@gmail.com>
2023-10-11 15:52:58 -0300

    Actualizado

--- Prog_Estructurada/l- Arrays-Matrices/main.c
+++ Prog_Estructurada/l- Arrays-Matrices/main.c
@@ -32,5 +32,26 @@ int main()
     printf("\nArray 3 con metodo general de recorrida:\n");
     for (int i = 0; i < (int) (sizeof(array_tres)/sizeof(array_tres[0])); i++){
         printf("%d ", array_tres[i]);
+    }printf("\n");
+
+
+    //podemos crear una matriz directamente pasando los valores
+    int matriz[3][9]={{1,1,1,1,1,0,1,1,1},
+                    {0,1,0,1,0,1,0,1,0},
+                    {1,1,1,1,1,0,1,1,1}
+                    };
+    int matriz2[3][9]= {1,1,1,1,1,0,1,1,1,
+                    0,1,0,1,0,1,0,1,0,
+                    1,1,1,1,1,0,1,1,1
+                    };
+
+    //for para recorrerlas con formato de filas y columnas
+    for (int filas = 0; filas <3; filas++){
+        for (int columnas= 0; columnas <9; columnas++){
+            printf("%d ", matriz2[filas][columnas]);
+        }
+        printf("\n");
     }
+//SE PUEDEN DECLARAR ARRAYS CON CONST, ejemplo CONST char hex_chars[] ya que de esta manera no se podrá modificar el array por el programa más adelante, e informarà errores si se intenta hacerlo
+
 }
