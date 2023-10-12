dbac7d644c7aa8ec917d63ab7dbaf194325b25b2
Lucas <lucas.castro.0307@gmail.com>
2023-10-12 18:05:05 -0300

    Actualizado Funciones

---
+++ Prog_Estructurada/m - Funciones/main.c
@@ -0,0 +1,63 @@
+//La estructura de la declaración de una función incluye al principio el tipo de dato que devuelve (sino un void)
+    //el nombre de la función, y luego entre paréntesis los tipos de datos esperados
+    //
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> //para usar tipo booleano
+
+//Debemos agregar el prototipo de la función para que el compilador las reconozca como tal
+double promedio (double a, double b);
+void mensaje();
+bool es_primo(int numero);
+void mostrar_vector(int cant,int a[]);
+
+
+int main()
+{
+    int valores[] = {3,4,5,6,7,8,3,2,1,6};
+    int num;
+    mensaje();
+    scanf("%d",&num);
+    if(es_primo(num)){
+        printf("%d es primo", num);
+    }else{
+        printf("%d no es primo", num);
+    }
+
+
+    printf("\nLos valores del vector son: ");
+    mostrar_vector(10,valores);
+    return 0;
+}
+
+
+double promedio (double a, double b){
+    return (a+b)/2;
+}
+
+//no devuelve nada
+void mensaje(){
+    printf("Ingrese el numero a analizar para ver si es primo\n");
+}
+
+
+bool es_primo(int numero){
+    bool primo = true;
+    for (int i = 2 ; i < numero;i ++){
+        if (numero%i == 0){
+            primo=false;
+        }
+    }
+    return primo;
+}
+
+
+void mostrar_vector( int cant,int a[]){
+    for (int i = 0; i < cant; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+}
+
+//en el caso de que querramos recorrer un aray dentro de una funcion, la función no sabe  la longitud del array, por lo que sizeof no sirve, hay que pasar como parámetro la longitud.
