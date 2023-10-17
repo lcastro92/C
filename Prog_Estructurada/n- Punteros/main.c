b9a4ace3f893e513849357418f838f0c2d9f69f5
Lucas <lucas.castro.0307@gmail.com>
2023-10-17 14:03:23 -0300

    Actualizado

--- Prog_Estructurada/n- Punteros/main.c
+++ Prog_Estructurada/n- Punteros/main.c
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void max_min (int vector[], int longitud, int *maximo, int *minimo);
+
 int main()
 {
 
     //para declarar punteros antecedemos un asterisco al nombre de la variable
-    int var = 5, *p = &var; //puntero a variable entera
+    int var = 5, *p = &var, vector[]={7,2,0,-2,50}, minimo, maximo; //puntero a variable entera
     double *q; //puntero a double
     char *r ;//puntero a char
 
@@ -19,8 +21,24 @@ int main()
     printf("Nuevo valor de *p = %d\n", *p);
     //y viceversa
     *p = 8;
-    printf("Nuevo valor de var = %d", var);
+    printf("Nuevo valor de var = %d\n", var);
 
-
+    max_min(vector, 5, &maximo, &minimo);
+    printf("El maximo es %d\n", maximo);
+    printf("El minimo es %d\n", minimo);
     return 0;
 }
+
+
+void max_min (int vector[], int longitud, int *maximo, int *minimo){
+    for (int i = 0; i < longitud; i++){
+        if (*maximo < vector[i])
+                //printf("Nuevo máximo: %d", vector[i]);
+                *maximo = vector[i];
+        if (*minimo > vector[i])
+                *minimo = vector[i];
+    }
+
+}
+
+
